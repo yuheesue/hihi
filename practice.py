304495fc5465e3ada86f23e3c53586a99ed50691
YHS <yhsue27@naver.com>
2021-05-05 23:26:00 +0900

    11-2패키지 모듈 추가파일

--- practice.py
+++ practice.py
@@ -1088,10 +1088,16 @@ price(6)'''
 # trip_to = travel.thailand.ThailandPackage()
 # trip_to.detail()
 
-from travel.thailand import ThailandPackage
-trip_to = ThailandPackage()
-trip_to.detail()
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
 
-from travel import vietnam
-trip_to = vietnam.VietnamPackage()
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+#from random import * 로 쓴적있음
+from travel import * #
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackage()
 trip_to.detail()
