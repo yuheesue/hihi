99c88fdeea0cdfa5fcfaf1c77e9b96a64304c8cd
YHS <yhsue27@naver.com>
2021-05-08 20:21:25 +0900

    list에는 find함수 사용 불가능

---
+++ lotto_high and low.py
@@ -0,0 +1,37 @@
+def solution(lottos, win_nums):
+    
+
+    
+    win_nums.find(lottos[0])
+    if True:
+        i += 1
+    
+    win_nums.find(lottos[1])
+    if True:
+        i += 1
+
+    win_nums.find(lottos[2])
+    if True:
+        i += 1
+
+    win_nums.find(lottos[3])
+    if True:
+        i += 1
+
+    win_nums.find(lottos[4])
+    if True:
+        i += 1
+
+    win_num.find(lottos[5])
+    if True:
+        i += 1
+
+    high = 7 - (win_num.count("0")+i)
+    low = 7 - i
+    answer = [ low , high ]
+
+    return answer
+
+a = [44,1,0,0,31,25]
+b = [31,10,45,1,6,19]
+print(solution(a,b))
---
+++ problem2.py
@@ -0,0 +1,12 @@
+def solution(places):
+    
+    place0 = places[0]
+    place1 = places[1]
+    place2 = places[2]
+    place3 = places[3]
+    place4 = places[4]
+
+    for n in place.count("P"):
+        a+n = place0.index("P")
+    answer = []
+    return answer
