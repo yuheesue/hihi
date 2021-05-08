43f9f8a193505ec2c87ff5abc757651f9dedf75b
YHS <yhsue27@naver.com>
2021-05-08 13:44:22 +0900

    불필요한 폴더 및 파일 삭제

--- byme.py
+++
@@ -1,5 +0,0 @@
-
-def sign():
-    print("이 프로그램은 나도코딩에 의해 만들어졌습니다.")
-    print("유투브 : http://")
-    print("이메일 : gmail.com")
--- help.py
+++
@@ -1,4 +0,0 @@
-print("helo")
-#heo
-#jhi
-print("너 제대로 작동하는거니,,!?!?!")
