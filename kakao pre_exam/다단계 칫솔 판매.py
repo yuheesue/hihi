1be836e7d7afe3f9fc485f1202cdefe712e1c684
YHS <yhsue27@naver.com>
2021-05-10 21:00:55 +0900

    약간 헛점을 고쳤으나 fail

--- kakao pre_exam/다단계 칫솔 판매.py
+++ kakao pre_exam/다단계 칫솔 판매.py
@@ -33,15 +33,20 @@ def solution(enroll, referral, seller, amount):
     for a in range(8):
 
         if enroll_money[a] != 0:
-            interest = int(answer[a] * 0.1)  #이자율
-            answer[a] -= interest   #이자율 뺀 금액
+            interest = int(answer[a] * 0.1)
+            if interest >=1:  #이자율
+                answer[a] -= interest   #이자율 뺀 금액
 
             while(interest>=1):    
                 if referral[a] != "-": #b변수를 이용하기 위해 
                     b = enroll.index(referral[a]) #referral을 enroll로 옮기기 위한 위치찾기
-                    answer[b] += interest - int(interest*0.1)
-                    interest = int(interest *0.1)
-                    a = b
+                    if interest*0.1 >=1:
+                        answer[b] += (interest - int(interest*0.1))
+                        interest = int(interest *0.1)
+                        a = b
+                    else:
+                        answer[b] += interest
+                        break
 
                 else:
                     break
