bcfdce7fa035a7da6e78d57216fb38c32127b02e
YHS <yhsue27@naver.com>
2021-05-11 02:16:00 +0900

    answer가 늘어나는것을 확인. 수정중

--- kakao pre_exam/다단계 칫솔 판매.py
+++ kakao pre_exam/다단계 칫솔 판매.py
@@ -1,18 +1,35 @@
 def solution(enroll, referral, seller, amount):
+    enroll_plus = []
+    referral_plus = []
+    answer_plus = []
     answer =[]
 
     for m in range(len(enroll)):
         if seller.count(enroll[m])==1:
-            index2 = seller.index(enroll[m])
-            answer.append(amount[index2]*100)
-                #anaswer[m] = enroll_money[m]
+            index = seller.index(enroll[m])
+            answer.append(amount[index]*100)
+            
+        elif seller.count(enroll[m])>1:
+            answer[m] = []  #
+            index = seller.index(enroll[m])
+            answer[m].append(amount[index]*100)#
+
+            for i in range(seller.count(enroll[m])-1):
+                index = seller.index(enroll[m], index)
+                answer[m].append(amount[index]*100)#
+                enroll_plus.append(enroll[m])
+                referral_plus.append(referral[m])
+
         else:
             answer.append(0)
 
-    
-    for a in range(len(enroll)):
+    enroll.extend(enroll_plus)
+    referral.extend(referral_plus)
+    answer.extend(answer_plus)
 
-        if answery[a] != 0:
+    for a in range(len(answer)):
+
+        if answer[a] != 0:
             interest = int(answer[a] * 0.1)
             if interest >=1:  #이자율
                 answer[a] -= interest   #이자율 뺀 금액
@@ -34,13 +51,13 @@ def solution(enroll, referral, seller, amount):
         else:
             continue
             
-        
+        print(enroll)
     return answer
 
 q1 = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
 q2 = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
-q3 = ["young", "john", "tod", "emily", "mary"]
-q4 = [12, 4, 2, 5, 10]
+q3 = ["young", "john", "tod", "emily", "mary", "mary", "mary","mary", "null" ]
+q4 = [12, 4, 2, 5, 10, 10, 10, 10, 0 ]
 
 
 print(solution(q1, q2, q3, q4))
