b5b568f97ddc8b707f34fa667619415dea096396
YHS <yhsue27@naver.com>
2021-05-17 01:50:16 +0900

    첫번째 for문에서 꼬임

--- basic_grammer.py
+++ basic_grammer.py
@@ -60,7 +60,7 @@ print(int(random() * 10)) #0 ~ 10 이하의 임의의 값 생성
 print(int(random() * 10) + 1) #1 ~ 10 이하의 임의의 값 생성
 print(int(random() * 45) + 1) #1 ~ 45 이하의 임의의 값 생성
 
-print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+print(rand(1,46)) # 1 ~ 46 미만의 임의의 값 생성
 
 print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
 
