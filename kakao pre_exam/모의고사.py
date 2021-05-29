4201f28528ff1d771a8e8b456d9e244f175c1e6c
YHS <yhsue27@naver.com>
2021-05-29 17:08:46 +0900

    각 수포자들 문제수만큼 배열늘림.

--- kakao pre_exam/모의고사.py
+++ kakao pre_exam/모의고사.py
@@ -1,36 +1,17 @@
 def solution(answers):
 
     p = 0
-    n = 0
-    correct = 0
+    i = 0
     answer = []
     math1 = [1,2,3,4,5]
     math2 = [2,1,2,3,2,4,2,5]
     math3 = [3,3,1,1,2,2,4,4,5,5]
 
-    while( p == len(answer)):
+    while( p+5 == len(answer)):
         
-        math1[p] = p%5+1
-        
-        if p%2 == 0:
-            math2[p] = 2
-        elif p%8 == 1:
-            math2[p] = 1
-        elif p%8 == 3:
-             math2[p] = 3
-        elif p%8 == 5:
-             math2[p] = 4
-        elif p%8 == 7:
-             math2[p] = 5 
-
-        math3.append(math[p])       
-
-
-            math2[p] =  p%8
-            1,9,17
-            4,12,8
-        if
-
+        math1.append(math1[p])
+        math2.append(math2[p])
+        math3.append(math3[p])       
 
     return answer
 
