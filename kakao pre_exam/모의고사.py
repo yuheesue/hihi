13d85d0fa8fb9678404261ed891d1acbbd6f695c
YHS <yhsue27@naver.com>
2021-05-30 12:51:26 +0900

    clear. 좀 더 pythonic하게 해보도록하자!

--- kakao pre_exam/모의고사.py
+++ kakao pre_exam/모의고사.py
@@ -2,18 +2,40 @@ def solution(answers):
 
     p = 0
     i = 0
+    a = 0
+    b = 0
+    c = 0
     answer = []
     math1 = [1,2,3,4,5]
     math2 = [2,1,2,3,2,4,2,5]
     math3 = [3,3,1,1,2,2,4,4,5,5]
 
-    while( p+5 == len(answer)):
+    while p+5 < len(answers):
         
         math1.append(math1[p])
         math2.append(math2[p])
-        math3.append(math3[p])       
+        math3.append(math3[p])
+        p += 1       
+
+    for i in range(len(answers)):
+
+        if math1[i] == answers[i]:
+            a += 1
+
+        if math2[i] == answers[i]:
+            b += 1
+
+        if math3[i] == answers[i]:
+            c += 1
+    
+    if max(a,b,c) == a:
+        answer.append(1)
+    if max(a,b,c) == b:
+        answer.append(2)
+    if max(a,b,c) == c:
+        answer.append(3)
 
     return answer
-
-    1,2,3,4,5,6,7,8,9,10
-    0,5,10,
+    
+answers = [1,3,2,4,2]
+print(solution(answers))
