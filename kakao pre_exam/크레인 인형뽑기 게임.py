99dd795bce8c7b587d87e40420d2c4b311a30a82
YHS <yhsue27@naver.com>
2021-06-10 18:18:34 +0900

    board에서 인형유무 과정 오류

--- kakao pre_exam/크레인 인형뽑기 게임.py
+++ kakao pre_exam/크레인 인형뽑기 게임.py
@@ -3,7 +3,29 @@ def solution(board, moves):
     answer = 0
 
     #moves위치 인식후 해당자리 집게이동
-    for a in moves:
-        colum = [ i [moves[a]]for i in board]
+    for a in range(len(moves)):
+        doll = moves[a]-1
+        print(doll)
+        colum = [ i [doll] for i in board]
+        print(colum)
+
+    #인형 구별
+        for b in colum:
+
+            if colum[b] != 0:
+                basket.append(colum[b]) 
+                colum[b] = 0
+                print(colum)
+                print(basket)
+                break
+
+        if basket[-1:] == basket[-2:-1]:
+            basket.pop()
+            basket.pop()
+            answer +=2
 
     return answer
+
+board =[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+print(solution(board, moves))
